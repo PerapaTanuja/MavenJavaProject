a68472fef5b38c9e71169a49942b6ad9d44f0814
Perapa Tanuja <perapatanuja22@gmail.com>
2022-11-21 19:53:37 +0530

    First commit to maven java project

---
+++ src/main/java/SEclassGroup/MavenJavaProject6699/App.java
@@ -0,0 +1,13 @@
+package SEclassGroup.MavenJavaProject6699;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is first Maven Java Priject in Eclipse" );
+    }
+}
